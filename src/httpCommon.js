5b288e942948931001092f8cdd07efd97242f863
Hyunjoon <joonkim0625@gmail.com>
2020-07-01 09:43:33 -0500

    change http baseUrl

--- src/httpCommon.js
+++ src/httpCommon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://joonkim.netlify.app",
+  baseURL: "https://joonkim.netlify.app/api",
   headers: {
     "Content-tpye": "application/json",
   },
